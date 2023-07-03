3250f2b7b7e0de2d4c994a05adccb5efe6f28f73
muno777 <schriskii@gmail.com>
2023-07-03 07:13:09 -0500

    Update node_2d.gd

--- Scenes/node_2d.gd
+++ Scenes/node_2d.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	get_window().size *= 2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
